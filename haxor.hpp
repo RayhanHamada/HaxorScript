74879d10f141731ba998678442fccb30f3e2bb4c
RayhanHamada <hamadadesk1999@gmail.com>
2020-05-10 16:28:41 +0700

    commit pertama gaannn

---
+++ haxor.hpp
@@ -0,0 +1,18 @@
+#ifndef HAXOR_H
+#define HAXOR_H
+
+#include <iostream>
+
+#define jadi_begini_gan {
+#define begini_gan {
+#define begitu_gan }
+#define jadi_begitu_gan }
+#define begitu_gan_biar_mengerti }
+#define jadi_begitu_gan_biar_mengerti }
+#define gan ;
+#define gaann ;
+#define mulai_gaann main
+#define mulai_gan main
+#define endl_gan << " gaann :)\n"; 
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,27 @@
+// haxor.hpp nya jangan lupa di include gaaannn ☺️☺️
+#include "./haxor.hpp"
+
+void halo();
+
+int mulai_gaann()
+  jadi_begini_gan
+    
+    std::cout << "Syntax khusus ini" endl_gan
+    std::cout << "dibuat agar elooo mengerti" endl_gan
+    halo() gan
+
+  begitu_gan_biar_mengerti
+
+// contoh deklarasi fungsi gaann ☺☺
+void halo() 
+    jadi_begini_gan
+
+        // contoh statement loop dan if gaaannn
+        for (int hacker = 0 gan hacker < 10 gan hacker++) 
+        begini_gan
+            std::cout << "hacker ke-" << hacker + 1 endl_gan
+            if (hacker == 7)
+                break gaann
+        begitu_gan
+    
+    jadi_begitu_gan_biar_mengerti
